33b1d521e39ebb87db107de04bfdd4a565a16e18
somm12 <>
2022-03-06 02:15:30 +0000

    baekjoon alogorithm 15686.py

--- algorithm/implementation/15686.py
+++ algorithm/implementation/15686.py
@@ -1,4 +1,6 @@
-n, M = map(int, input().split())
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+n,M = map(int, input().split())
 arr = [[0]*n for i in range(n)]
 
 chicken=[]
@@ -18,19 +20,38 @@ for i in range(n):
 
 chicken_distance = []
 
+val = 0
 for i in range(len(chicken)):
-  for k in range(len(house)):
-    add = abs(chicken[i][0]-house[k][0]) + abs(chicken[i][1]-house[i][1])
-    chicken_distance.append((add,i))
+    for k in range(len(house)):
+        add = abs(chicken[i][0]-house[k][0]) + abs(chicken[i][1]-house[k][1])
+        val += add
+    chicken_distance.append((val,i))
+    val = 0
 
 chicken_distance.sort()
-chicken_distance = chicken_distance[:M+1]
+
+print("치킨거리")
+print(chicken_distance)
+chicken_distance = chicken_distance[:M]
 #치킨거리가 젤 적은 치킨집 찾고
 #치킨거리가 작은 치킨집의 index찾기
 
+print("치킨집 위치 ")
+print(chicken)
+print("집위치 ")
+print(house)
+print("치킨거리 작은 거 + 인덱스 ")
+print(chicken_distance)
+
+
+
 last_chicken = []
 for i in range(len(chicken_distance)):
-  last_chicken.append(chicken.pop(chicken_distance[i][1]))
+    print(chicken_distance[i][1])
+
+    last_chicken.append(chicken[chicken_distance[i][1]])
+
+print(last_chicken)
 
 #치킨거리 합을 구하기
 result = 0
@@ -40,6 +61,7 @@ for i in range(len(house)):
     add = abs(house[i][0]-last_chicken[k][0]) + abs(house[i][1]-last_chicken[k][1])
     find_min_value.append(add)
   result+=min(find_min_value)
+  find_min_value = []
 
 print(result)
 
