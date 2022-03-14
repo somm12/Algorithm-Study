586fbe9b2b79725e817b1a835f6c8d7ed96198d0
somm12 <thals8744@naver.com>
2022-03-14 22:42:16 +0900

    baekjoon 2178.py

--- algorithm/bfs/2178.py
+++ algorithm/bfs/2178.py
@@ -1,13 +1,49 @@
+from collections import deque
+
+
 N,M = map(int, input().split())
 arr = []
+count = 1
 #input with no space,, how?
 for i in range(N):
     a = list(input())
     arr.append(a)
 
-print()
-
 for i in range(N):
     for k in range(M):
-        print(arr[i][k], end='')
-    print()
+        if arr[i][k] == 1:
+            arr[i][k] = count
+        count +=1
+
+graph = [[]]
+visited = [False]*(count-1)
+for i in range(N):
+    for k in range(M):
+        if arr[i][k] != 0:
+            a = []
+            if arr[i-1][k] != 0 and i >0:
+                a.append(arr[i-1][k])
+            elif arr[i][k-1] != 0 and k >0:
+                a.append(arr[i][k-1])
+            elif arr[i][k+1] != 0 and k < M -1:
+                a.append(arr[i][k+1])
+            elif arr[i+1][k] != 0 and i < N-1:
+                a.append(arr[i+1][k])
+        graph.append(a)
+depth = 0
+def bfs(graph, start, visited):
+    queue = deque([start])
+    visited[start] = True
+    while queue:
+        v = queue.popleft()
+        print(v, end='')
+        depth = depth + 1
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+
+bfs(graph,1,visited)
+
+print(count)
+
