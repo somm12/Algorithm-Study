056b0c8e9c5fadd78496ea64f8a2ca704b485f3c
somm12 <thals8744@naver.com>
2022-03-17 12:17:59 +0900

    baekjoon 11724.py ~ing no time over, wrong ~ing

--- algorithm/bfs/11724.py
+++ algorithm/bfs/11724.py
@@ -1,33 +1,30 @@
 from collections import deque
+import sys
+sys.setrecursionlimit(10**6)
 
-N,M = map(int,input().split())
+N,M = map(int,sys.stdin.readline().rstrip().split())
 graph =[[] for i in range(N+1)]
 for i in range(M):
-    a,b = map(int,input().split())
+    a,b = map(int,sys.stdin.readline().rstrip().split())
     graph[a].append(b)
-                   
+    graph[b].append(a)
+
+
+                 
 def bfs(graph,start, visited):
-    queue = deque([start])
-    if visited[start] == True:
-        return 0
-    else:
-        visited[start] = True
-        while queue:
-            v = queue.popleft()
-            if v >= N:
-                visited[v] = True
-                continue
-            else:
-                for i in graph[v]:
-                    if not visited[i]:
-                        queue.append(i)
-                        visited[i] = True
-        return 1
+    visited[start] = True
+    for i in graph[start]:
+        if not visited[i]:
+            visited[i] = True
+        for k in graph[i]:
+            if not visited[k]:
+                visited[k]= True
 
 visited = [False] * (N+1)
-
-result = 0
+visited[0] =True
+count = 0
 for i in range(1,N+1):
-    result = result + bfs(graph,i,visited)    
-print(result)
-
+    if (visited[i] == False):
+        bfs(graph,i,visited)
+        count += 1
+print(count)
