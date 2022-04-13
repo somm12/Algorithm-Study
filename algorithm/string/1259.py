a6573a8acac388559c95efd776c78ea3dd720edd
somm12 <thals8744@naver.com>
2022-04-13 09:51:52 +0900

    baekjoon algorithm 1259.py pelindrome

---
+++ algorithm/string/1259.py
@@ -0,0 +1,11 @@
+import sys
+while True:
+    arr = sys.stdin.readline().strip()
+    if int(arr) == 0:
+        break
+    arr = list(arr)
+    reversed_arr = arr[::-1]
+    if arr == reversed_arr:
+        print("yes")
+    else:
+        print("no")
