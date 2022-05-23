7bdc7f5fa95dbc0fab59fb89754ad454344dbc93
somm12 <sominan8744@gmail.com>
2022-05-23 20:01:23 +0900

    baekjoon algorithm 2525.pyu

---
+++ baekjoon/implementation/2525.py
@@ -0,0 +1,13 @@
+hour, minute = map(int,input().split())
+
+time = int(input())
+
+updateMinute = minute + time
+if updateMinute >= 60:
+    hour = hour + (updateMinute // 60)
+    updateMinute = updateMinute % 60
+    if hour > 23:
+        hour = hour - 24
+
+print(hour,end=' ')
+print(updateMinute)
