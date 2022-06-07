1d3879324e9a218daaae5cced06edf2ed2ca8879
somm12 <sominan8744@gmail.com>
2022-06-07 16:13:18 +0900

    baekjoon algorithm 13305.py changed code

--- baekjoon/greedy/13305.py
+++ baekjoon/greedy/13305.py
@@ -3,18 +3,15 @@ from cmath import inf
 
 numOfCity = int(input())
 cityDistance = list(map(int,input().split()))
-price = list(map(int,input().split()))
+cost = list(map(int,input().split()))
 
 result = 0
-min = inf
-for i in range(numOfCity-1):
-    for j in range(i+1):
-        if j == i:
-            result += price[j] * sum(cityDistance[i:])
-        else:
-            result += price[j] * cityDistance[j]
-    if min > result:
-        min = result
-    result = 0
+lowerCost = inf
 
-print(min)
+for i in range(numOfCity - 1):
+    if cost[i] < lowerCost:
+        result += cost[i] * cityDistance[i]
+        lowerCost = cost[i]
+    else:
+        result += lowerCost * cityDistance[i]
+print(result)
