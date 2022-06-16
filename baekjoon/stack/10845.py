14de7dc6690447f5ec8cad0e6be8bda7e758c4e9
somm12 <sominan8744@gmail.com>
2022-06-16 10:32:06 +0900

    baekjoon 10845.py update

--- baekjoon/stack/10845.py
+++ baekjoon/stack/10845.py
@@ -1,33 +1,34 @@
 from collections import deque
-n = int(input())
+import sys
+n = int(sys.stdin.readline())
 q = deque()
 for i in range(n):
-    command = input().split()
+    command = sys.stdin.readline().split()
     if len(command) == 2:
         q.append(int(command[1]))
-        print(command[1])
  
     else:
         if command[0] == 'front':
-            if len(q) == 0:
-                print(0)
+            if q:
+                print(q[0])
             else:
-                print(q[-1])
-        elif command[0] == 'back':
-            if len(q) == 0:
                 print(-1)
-            else:
+        elif command[0] == 'back':
+            if q:
                 print(q[-1])
+            else:
+                print(-1)
         elif command[0] == 'size':
             print(len(q))
         elif command[0] == 'empty':
-            if len(q)== 0:
-                print(1)
-            else:
+            if q:
                 print(0)
-        elif command[0] == 'pop':
-            if len(q) == 0:
-                print(-1)
             else:
+                print(1)
+        elif command[0] == 'pop':
+            if q:
                 x = q.popleft()
                 print(x)
+                
+            else:
+               print(-1)
