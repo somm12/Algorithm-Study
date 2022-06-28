3697e0b7ec7e359f852d381e801f09cf336e6935
somm12 <sominan8744@gmail.com>
2022-06-29 07:42:12 +0900

    baekjoon algorithm 1065.py changed code to make a solution function

--- baekjoon/implementation/1065.py
+++ baekjoon/implementation/1065.py
@@ -1,19 +1,24 @@
-n = int(input())
 
-if n < 100:
-    ans = n
-else:
-    ans = 99
-    for i in range(100, n + 1):
-        # 각 자리수 숫자를 담는 배열 digit
-        digit = []
-        while i > 0:
-            digit.append(i % 10)
-            i = i // 10
-        d = set()
-        # 각 자리수의 차를 구해서 집합에 추가, 만약 하나라도 다른 차가 나온다면 한수에 해당X
-        for k in range(len(digit) - 1):
-            d.add(digit[k] - digit[k + 1])
-        if len(d) == 1:
-            ans += 1
-print(ans)
+def solution(n):
+    if n < 100:
+        ans = n
+    else:
+        ans = 99
+        for i in range(100, n + 1):
+            # 각 자리수 숫자를 담는 배열 digit
+            digit = []
+            while i > 0:
+                digit.append(i % 10)
+                i = i // 10
+            d = set()
+            # 각 자리수의 차를 구해서 집합에 추가, 만약 하나라도 다른 차가 나온다면 한수에 해당X
+            for k in range(len(digit) - 1):
+                d.add(digit[k] - digit[k + 1])
+            if len(d) == 1:
+                ans += 1
+    return ans
+
+
+if __name__ == "__main__":
+    n = int(input())
+    print(solution(n))
