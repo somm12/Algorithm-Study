468863f03a93ab95deb54ee444e879947822cd19
somm12 <sominan8744@gmail.com>
2022-07-07 13:02:34 +0900

    baekjoon algorithm 1018.py add more comments

--- baekjoon/bruteForce/1018.py
+++ baekjoon/bruteForce/1018.py
@@ -21,12 +21,13 @@ if __name__ == "__main__":
     # 보드판 입력 
     for i in range(n):
         arr.append(list(input()))
-       
+    # 어디를 시작점으로 8 x 8 체스판을 만들 수 있는지 모든 경우를 가져옴   
     for i in range(n - 7):
         for k in range(m - 7):
-            
+            # 체스판 가장 왼쪽 위쪽이 흰색이냐 검은색이냐 두 가지 경우
             case1 = chessMake(i,k,'W')
             case2 = chessMake(i,k,'B')
+            # 색을 바꿀 수 있는 더 작은 경우의 수를 할당.
             if case1 <= case2:
                 res = case1
             else:
