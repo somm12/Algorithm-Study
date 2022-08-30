3c9870cd19a49871bbbcd3bcc9acff8ac69d189c
somm12 <sominan8744@gmail.com>
2022-08-31 06:50:28 +0900

    baekjoon algorithm 15650.py update

---
+++ baekjoon/bruteForce/15650.py
@@ -0,0 +1,18 @@
+def dfs(L):
+    if L == m:
+        for j in range(L):
+            print(res[j],end=' ')
+        print()
+    else:
+        for i in range(1, n+1):
+            if ch[i] == 0 and i > res[L-1]:
+                ch[i] = 1
+                res[L] = i
+                dfs(L+1)
+                ch[i] = 0
+
+if __name__ == "__main__":
+    n, m = map(int,input().split())
+    res = [0] * n
+    ch = [0] * (n + 1)
+    dfs(0)
