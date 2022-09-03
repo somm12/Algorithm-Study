f1bffe0af1511f6d1a242cf57e9e020959350a0d
somm12 <sominan8744@gmail.com>
2022-09-04 06:52:31 +0900

    baekjoon algorithm 15655.py update

---
+++ baekjoon/bruteForce/15655.py
@@ -0,0 +1,19 @@
+def dfs(L):
+    if L == m:
+        for val in res:
+            print(val, end=' ')
+        print()
+    else:
+        for i in range(n):
+            if L == 0:
+                res[L] = arr[i]
+                dfs(L + 1)
+            elif arr[i] not in res[:L] and res[L - 1] < arr[i]:
+                res[L] = arr[i]
+                dfs(L + 1)
+if __name__ == "__main__":
+    n, m = map(int,input().split())
+    arr = list(map(int,input().split()))
+    arr.sort()
+    res = [0] * m
+    dfs(0)
