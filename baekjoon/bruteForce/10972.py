a01615431239ad4c29884946b1426c52de809d42
somm12 <sominan8744@gmail.com>
2022-09-11 08:10:13 +0900

    baekjoon algorithm 10972.py need to solve again

---
+++ baekjoon/bruteForce/10972.py
@@ -0,0 +1,15 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+for i in range(n - 1, 0, -1):
+    if arr[i] > arr[i - 1]:
+        x = i - 1
+        y = i
+        for j in range(n - 1, 0, -1):
+            if arr[x] < arr[j]:
+                arr[x], arr[j] = arr[j], arr[x]
+                new = arr[:y] + sorted(arr[y:])
+                for val in new:
+                    print(val, end=' ')
+                exit()
+print(-1)
