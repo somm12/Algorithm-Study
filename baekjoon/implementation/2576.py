660dc956f14fabb7f568effcf54d86e02a855757
somm12 <sominan8744@gmail.com>
2022-09-19 06:41:17 +0900

    baekjoon algorithm 2576.py add comments

--- baekjoon/implementation/2576.py
+++ baekjoon/implementation/2576.py
@@ -15,3 +15,16 @@ if cnt == 7:
 else:
     print(total)
     print(minV)
+# 홀수인 숫자만 배열에 추가하여 sum, min함수를 이용하는 방법.
+import sys
+
+res = []
+for _ in range(7):
+    a = int(sys.stdin.readline())
+    if a % 2 != 0:
+        res.append(a)
+if len(res) == 7:
+    print(sum(res))
+    print(min(res))
+else:
+    print(-1)
