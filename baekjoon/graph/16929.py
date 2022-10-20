be431cd3019e57ef6318f0fa4f5db8431d0e01ca
somm12 <sominan8744@gmail.com>
2022-10-21 06:42:10 +0900

    baekjoon algorithm 16929.py update once again completed

--- baekjoon/graph/16929.py
+++ baekjoon/graph/16929.py
@@ -1,38 +1,37 @@
 import sys
 input = sys.stdin.readline
+n, m = map(int, input().rstrip().split())
+graph = []
+for _ in range(n):
+    graph.append(list(input()))
 
-def dfs(color, x, y, cnt, start_x, start_y):
+visited = [[0] * m for _ in range(n)]
+ans = False
+dx = [-1,1,0,0,]
+dy = [0,0, -1,1]
+def sol(color, x, y, cnt, start_x, start_y):
     global ans
     if ans:
         return
     for i in range(4):
-        nx, ny = x + dx[i], y + dy[i]
-        if nx <0 or nx >= n or ny < 0 or ny >= m:
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if nx < 0 or nx >=n or ny < 0 or ny >= m:
             continue
-        if cnt >= 4 and nx == start_x and ny ==start_y:
+        if graph[nx][ny] == color and cnt >= 4 and start_x == nx and start_y == ny:
             ans = True
             return
         if graph[nx][ny] == color and visited[nx][ny] == 0:
             visited[nx][ny] = 1
-            dfs(color, nx, ny, cnt + 1, start_x, start_y)
+            sol(color, nx, ny, cnt + 1, start_x, start_y)
             visited[nx][ny] = 0
 
 
-n, m = map(int, input().split())
-graph = [[i for i in input().rstrip()] for _ in range(n)]
-
-visited = [[0] * m for _ in range(n)]
-
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-ans = False
-
 for i in range(n):
     for j in range(m):
-        start_x, start_y = i, j
-        visited[start_x][start_y] = 1
-        dfs(graph[i][j], i, j, 1, start_x,start_y)
-        if ans:
+        visited[i][j] = 1
+        sol(graph[i][j], i, j, 1, i, j)
+        if ans == True:
             print('Yes')
             exit()
-print('No') 
+print('No')
