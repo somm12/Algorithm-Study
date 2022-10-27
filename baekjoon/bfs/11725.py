71b3c6024dc23c12c4ee30a100498b111ada908c
somm12 <sominan8744@gmail.com>
2022-10-28 07:16:04 +0900

    baekjoon algorithm 11725.py update added comments

--- baekjoon/bfs/11725.py
+++ baekjoon/bfs/11725.py
@@ -1,9 +1,11 @@
 from collections import deque
-
+import sys
+input = sys.stdin.readline
 
 n = int(input())
 graph = [[] for _ in range(n + 1)]
-# 답을 담은 배열 answer가 방문표시 배열 역할을 동시에 한다.
+# 답을 담은 배열 answer가 방문표시 배열 역할을 동시에 할 수 있다.
+visited = [0] * (n + 1)
 answer = [0] * (n + 1)
 
 for _ in range(n-1):
@@ -12,12 +14,13 @@ for _ in range(n-1):
     graph[b].append(a)
 def bfs():
     q = deque([1])
-    answer[1] = 1
+    visited[1] = 1
     while q:
         x = q.popleft()    
         for i in graph[x]:
-            if not answer[i]:
+            if not visited[i]:
                 answer[i] = x
+                visited[i] = 1
                 q.append(i)
 
 bfs()
