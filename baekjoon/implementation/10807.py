e19c20e4ce39445b8092b3b0a61490edfc45cc08
somm12 <sominan8744@gmail.com>
2022-11-04 06:32:44 +0900

    baekjoon algorithm 10807.py update

---
+++ baekjoon/implementation/10807.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+arr = list(map(int, input().split()))
+v = int(input())
+count = 0
+for i in arr:
+    if i == v:
+        count += 1
+print(count)
