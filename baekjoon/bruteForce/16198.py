0329bdf276a3eb31a3a2df34152d56edd955b3ff
somm12 <sominan8744@gmail.com>
2022-11-09 07:39:26 +0900

    baekjoon algorithm 16198.py update & need to solve again

---
+++ baekjoon/bruteForce/16198.py
@@ -0,0 +1,23 @@
+from glob import glob
+import sys
+input = sys.stdin.readline
+n = int(input())
+arr = list(map(int, input().split()))
+maxV = -1
+res = 0
+
+def sol(x):
+    global maxV,res
+    if len(arr) == 2:
+        maxV = max(x, maxV)
+        return
+    else:
+        for i in range(1, len(arr) - 1):
+            res = arr[i-1] * arr[i+1]
+            v = arr.pop(i)
+            sol(x + res)
+            arr.insert(i,v)
+
+sol(0)
+print(maxV)
+# 누적해서 더하는 것이 있다면, 매개변수를 이용한다.
