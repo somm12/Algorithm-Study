27ef32da25a440c79a4d134833480531d889bbba
somm12 <sominan8744@gmail.com>
2022-11-11 06:09:46 +0900

    baekjoon algorithm 16198_2.py update

--- baekjoon/bruteForce/16198_2.py
+++ baekjoon/bruteForce/16198_2.py
@@ -1,22 +1,17 @@
-from glob import glob
 import sys
 input = sys.stdin.readline
 n = int(input())
 arr = list(map(int, input().split()))
 maxV = -1
-add = 0
-
-def sol(x):
+def sol(ans):
     global maxV
     if len(arr) == 2:
-        maxV = max(x, maxV)
+        maxV = max(maxV, ans)
         return
-    else:
-        for i in range(1, len(arr) - 1):
-            add = arr[i-1] * arr[i+1]
-            v = arr.pop(i)
-            sol(x + add)
-            arr.insert(i,v)
-
+    for i in range(1, len(arr) - 1):
+        temp = arr[i - 1] * arr[i + 1]
+        a = arr.pop(i)
+        sol(ans + temp)
+        arr.insert(i, a)
 sol(0)
 print(maxV)
